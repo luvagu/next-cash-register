3502a30dd605c74c42babb25aa37c328e1769791
luvagu <luiavag@gmail.com>
2022-01-23 13:37:03 -0500

    added initial config data

---
+++ config/index.js
@@ -0,0 +1,52 @@
+export const TRANSACTIONS = [
+	{
+		name: 'Notas Venta',
+		variant: 'default',
+		operation: '+',
+	},
+	{
+		name: 'Facturas',
+		variant: 'default',
+		operation: '+',
+	},
+	{
+		name: 'Fact+Ret',
+		variant: 'default',
+		operation: '+',
+	},
+	{
+		name: 'Fact+TC',
+		variant: 'default',
+		operation: '+',
+	},
+	{
+		name: 'Cobros',
+		variant: 'default',
+		operation: '+',
+	},
+	{
+		name: 'Dev Ret',
+		variant: 'default',
+		operation: '-',
+	},
+	{
+		name: 'Gastos',
+		variant: 'default',
+		operation: '-',
+	},
+]
+
+export const PAYMENT_METHODS = [
+	{
+		name: 'Efectivo',
+		variant: 'default',
+	},
+	{
+		name: 'Cheque',
+		variant: 'default',
+	},
+	{
+		name: 'Tarjeta',
+		variant: 'default',
+	},
+]
