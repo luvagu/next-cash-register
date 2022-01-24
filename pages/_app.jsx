b3889a661bb26b553ebf04e3f51d9cdbf21e7efb
luvagu <luiavag@gmail.com>
2022-01-23 21:50:37 -0500

    added RegisterProvider state wrapper

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,12 @@
+import RegisterProvider from '../contexts/RegisterProvider'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<RegisterProvider>
+			<Component {...pageProps} />
+		</RegisterProvider>
+	)
 }
 
 export default MyApp
