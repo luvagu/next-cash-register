437bbc25f397d94e5e447d590a27e9b94541db98
luvagu <luiavag@gmail.com>
2022-01-27 13:50:54 -0500

    testing implementation

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useRef } from 'react'
+import { Fragment, useEffect, useRef } from 'react'
+import { FaCashRegister } from 'react-icons/fa'
 import Layout from '../components/Layout'
 import RadioItemsGroup from '../components/RadioItemsGroup'
 import RadioOptionsGroup from '../components/RadioOptionsGroup'
 import { ACTIONS, useRegister } from '../contexts/RegisterProvider'
-import { classNames } from '../utils/helpers'
+import { classNames, curencyFormatter } from '../utils/helpers'
 
 export default function Home() {
 	const { state, dispatch } = useRegister()
@@ -15,32 +16,44 @@ export default function Home() {
 		selectedTransaction,
 		selectedPaymentMethod,
 		transactions,
-		paymentMethods,
-		denominations,
+		transactionPaymentMethods,
+		paymentMethodTenders,
+		selectedTenderOption,
 	} = state
 
-	console.log(state)
-
 	const trAmountRef = useRef(null)
-	const exactRef = useRef(null)
 	const pmAmountRef = useRef(null)
 
 	useEffect(() => {
-		trAmountRef.current.value = undefined
+		if (trAmountRef.current) {
+			trAmountRef.current.value = undefined
+		}
 	}, [selectedTransaction?.name])
 
 	useEffect(() => {
-		// exactRef.current.checked = false
-		pmAmountRef.current.value = undefined
+		if (pmAmountRef.current) {
+			pmAmountRef.current.value = undefined
+		}
 	}, [selectedPaymentMethod?.name])
+
+	useEffect(() => {
+		if (pmAmountRef.current) {
+			const id = selectedTenderOption.id
+			const amount = selectedTransaction?.amount
+			const tenderAmount = selectedTenderOption?.value
+			pmAmountRef.current.value =
+				id === 'other' ? undefined : id === 'exact' ? amount : tenderAmount
+		}
+	}, [selectedTenderOption, selectedTransaction?.amount])
 
 	return (
 		<Layout>
-			<div className='grid grid-cols-1 md:grid-cols-[auto_minmax(200px,_300px)] gap-2 sm:gap-4 items-start'>
-				<div className='order-2 md:order-1 flex flex-col gap-2 sm:gap-4'>
-					<h1 className='text-base sm:text-lg font-semibold'>
+			<div className='grid grid-cols-[auto_350px] gap-2 sm:gap-4 items-start h-full'>
+				<div className='flex flex-col gap-2 sm:gap-4 p-4'>
+					{/* step 1 */}
+					<h2 className='text-base sm:text-lg font-semibold'>
 						1. Seleccionar transacción
-					</h1>
+					</h2>
 					<RadioItemsGroup
 						items={transactions}
 						getSelected={transaction =>
@@ -50,25 +63,28 @@ export default function Home() {
 							})
 						}
 					/>
-					<h1 className='text-base sm:text-lg font-semibold'>
+
+					{/* step 2 */}
+					<h2 className='text-base sm:text-lg font-semibold'>
 						2. Ingresar monto de transacción
-					</h1>
+					</h2>
 					<div className='relative max-w-xs rounded shadow-md'>
 						<div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
-							<span className='text-gray-500 text-sm sm:text-base'>$</span>
+							<span className='text-slate-500 text-sm sm:text-base'>$</span>
 						</div>
 						<input
 							ref={trAmountRef}
 							className={classNames(
-								'block w-full border-0 rounded pl-7 pr-3 text-sm sm:text-base text-gray-900 font-semibold bg-white focus:outline-none focus:ring-2 focus:ring-slate-900/50 disabled:bg-slate-300 disabled:bg-opacity-75 disabled:shadow-none invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500',
+								'block w-full border-0 rounded pl-7 pr-3 text-sm sm:text-base text-slate-900 font-semibold bg-white focus:outline-none focus:ring-4 focus:ring-slate-900/50 disabled:bg-slate-300/75 disabled:shadow-none invalid:text-red-600 focus:invalid:border-red-500 focus:invalid:ring-red-500',
 								!isTransactionSelected
-									? 'disabled:text-gray-500'
-									: isPaymentMethodSelected && 'disabled:text-gray-700'
+									? 'disabled:text-slate-500'
+									: isPaymentMethodSelected && 'disabled:text-slate-700'
 							)}
 							type='number'
 							min={0}
 							step={0.01}
 							placeholder='0.00'
+							value={selectedTransaction?.amount}
 							disabled={!isTransactionSelected || isPaymentMethodSelected}
 							required
 							onChange={e =>
@@ -79,84 +95,120 @@ export default function Home() {
 							}
 						/>
 					</div>
-					<h1 className='text-base sm:text-lg font-semibold'>
-						3. Seleccionar forma de pago
-					</h1>
-					<RadioItemsGroup
-						items={paymentMethods}
-						getSelected={paymentMethod =>
-							dispatch({
-								type: ACTIONS.SET_PAYMENT_METHOD,
-								payload: paymentMethod,
-							})
-						}
-						isGroupDisabled={!isSetTransactionAmount}
-					/>
-					<h1 className='text-base sm:text-lg font-semibold'>
-						4. Ingresar monto de pago
-					</h1>
-					{/* <div className='flex flex-col gap-2'>
-						<div className='flex items-center'>
-							<input
-								ref={exactRef}
-								id='exact-amount'
-								type='checkbox'
-								className='h-4 w-4 text-gray-900 border-slate-500 rounded focus:ring-slate-900/75 disabled:border-slate-400
-								disabled:bg-slate-300/75'
-								disabled={!isPaymentMethodSelected}
-								onChange={e => {
-									const amount = e.target.checked
-										? selectedTransaction?.amount
-										: undefined
+
+					{selectedTransaction && selectedTransaction?.hasPaymentMethods && (
+						<Fragment>
+							{/* step 3 */}
+							<h2 className='text-base sm:text-lg font-semibold'>
+								3. Seleccionar forma de pago
+							</h2>
+							<RadioItemsGroup
+								items={transactionPaymentMethods}
+								getSelected={paymentMethod =>
 									dispatch({
-										type: ACTIONS.UPDATE_PAYMENT_AMOUNT,
-										payload: { amount },
+										type: ACTIONS.SET_PAYMENT_METHOD,
+										payload: paymentMethod,
 									})
-									pmAmountRef.current.value = amount
-									pmAmountRef.current.disabled = e.target.checked
-								}}
+								}
+								isGroupDisabled={!isSetTransactionAmount}
 							/>
-							<label
-								htmlFor='exact-amount'
-								className='ml-2 block text-sm text-gray-900'
-							>
-								Monto de pago exacto?
-							</label>
-						</div>
-					</div> */}
-					<RadioOptionsGroup
-						items={denominations}
-						isGroupDisabled={!isPaymentMethodSelected}
-						selectedIndex={denominations.length - 1}
-					/>
-					<div className='relative max-w-xs rounded shadow-md'>
-						<div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
-							<span className='text-gray-500 text-sm sm:text-base'>$</span>
-						</div>
-						<input
-							ref={pmAmountRef}
-							className={classNames(
-								'block w-full border-0 rounded pl-7 pr-3 text-sm sm:text-base text-gray-900 font-semibold bg-white focus:outline-none focus:ring-2 focus:ring-slate-900/50 disabled:bg-slate-300 disabled:bg-opacity-75 disabled:shadow-none invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500',
-								!isTransactionSelected
-									? 'disabled:text-gray-500'
-									: isPaymentMethodSelected && 'disabled:text-gray-700'
+
+							{/* step 4 */}
+							{selectedPaymentMethod && (
+								<Fragment>
+									<h2 className='text-base sm:text-lg font-semibold'>
+										4. Ingresar monto de pago
+									</h2>
+									<RadioOptionsGroup
+										items={paymentMethodTenders}
+										getSelected={tender => {
+											dispatch({
+												type: ACTIONS.SET_TENDER_OPTION,
+												payload: tender,
+											})
+											dispatch({
+												type: ACTIONS.UPDATE_PAYMENT_AMOUNT,
+												payload: {
+													amount:
+														tender.id === 'other'
+															? undefined
+															: tender.id === 'exact'
+															? selectedTransaction?.amount
+															: tender?.value,
+												},
+											})
+										}}
+										isGroupDisabled={!isPaymentMethodSelected}
+										selectedItem={selectedTenderOption}
+									/>
+									<div className='relative max-w-xs rounded shadow-md'>
+										<div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
+											<span className='text-slate-500 text-sm sm:text-base'>
+												$
+											</span>
+										</div>
+										<input
+											ref={pmAmountRef}
+											className={classNames(
+												'block w-full border-0 rounded pl-7 pr-3 text-sm sm:text-base text-slate-900 font-semibold bg-white focus:outline-none focus:ring-4 focus:ring-slate-900/50 disabled:bg-slate-300/75 disabled:shadow-none invalid:text-red-600 focus:invalid:border-red-500 focus:invalid:ring-red-500',
+												!isTransactionSelected
+													? 'disabled:text-slate-500'
+													: isPaymentMethodSelected && 'disabled:text-slate-700'
+											)}
+											type='number'
+											min={selectedTransaction?.amount || 0}
+											step={0.01}
+											placeholder='0.00'
+											disabled={selectedTenderOption?.id !== 'other'}
+											required
+											onChange={e =>
+												dispatch({
+													type: ACTIONS.UPDATE_PAYMENT_AMOUNT,
+													payload: { amount: parseFloat(e.target.value) },
+												})
+											}
+										/>
+									</div>
+								</Fragment>
 							)}
-							type='number'
-							min={selectedTransaction?.amount || 0}
-							step={0.01}
-							placeholder='0.00'
-							disabled={!isPaymentMethodSelected}
-							required
-							onChange={e =>
-								dispatch({
-									type: ACTIONS.UPDATE_PAYMENT_AMOUNT,
-									payload: { amount: parseFloat(e.target.value) },
-								})
-							}
-						/>
-					</div>
+						</Fragment>
+					)}
 				</div>
-				<aside className='order-1 md:order-2 h-96 lg:h-full bg-amber-300'></aside>
+				<aside className='relative h-full bg-slate-300/75'>
+					<div className='absolute top-0 left-0 right-0 bg-slate-50/90 shadow-sm py-2 px-4 pb-4 flex flex-col gap-1'>
+						<h2 className='text-base sm:text-lg font-semibold'>
+							Transacción actual
+						</h2>
+						<div className='flex justify-between text-sm sm:text-base font-medium text-slate-900'>
+							<p>Total</p>
+							<p>{curencyFormatter(selectedTransaction?.amount)}</p>
+						</div>
+						<div className='flex justify-between text-sm sm:text-base font-medium text-slate-900'>
+							<p>Pagado</p>
+							<p>{curencyFormatter(selectedPaymentMethod?.amount)}</p>
+						</div>
+						<div className='h-1 mt-1 border-b-2 border-dashed border-slate-300' />
+						<div className='flex justify-between text-sm sm:text-base font-bold text-slate-900'>
+							<p>Cambio</p>
+							<p>{curencyFormatter(-52)}</p>
+						</div>
+						{/* final */}
+						<div className='mt-2'>
+							<button
+								type='button'
+								className='relative flex justify-center items-center gap-2 sm:gap-2 px-4 py-2 w-full max-w-xs rounded shadow-md text-sm sm:text-base text-slate-50 font-semibold bg-lime-700 focus:outline-none focus:bg-lime-800 focus:text-white focus:ring-4 focus:ring-slate-900/50 disabled:bg-slate-300/75 disabled:text-slate-500 disabled:shadow-none transition-colors'
+								disabled={
+									!(
+										selectedPaymentMethod?.amount >= selectedTransaction?.amount
+									)
+								}
+							>
+								<FaCashRegister />
+								<span>Finalizar</span>
+							</button>
+						</div>
+					</div>
+				</aside>
 			</div>
 		</Layout>
 	)
