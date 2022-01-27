a7f56c8be13a3b2fe523c5f530743465b31e4f91
luvagu <luiavag@gmail.com>
2022-01-27 13:50:39 -0500

    added more actions

--- contexts/RegisterProvider.jsx
+++ contexts/RegisterProvider.jsx
@@ -1,14 +1,15 @@
 import { createContext, useContext, useReducer } from 'react'
-import { PAYMENT_METHODS, TENDER_AMOUNTS, TRANSACTIONS } from '../config'
+import { PAYMENT_METHODS, TENDER_OPTIONS, TRANSACTIONS } from '../config'
 
 const RegisterContext = createContext()
 
 export const useRegister = () => useContext(RegisterContext)
 
 // TODO's:
-// - disable payment methods when payment amount is enetered or exact amount checkbox is checked
-// - add radio options for payment amounts [exact / 25c / 50c / 1 / 5 / 10 / 20 / other] and disable option if less than amount
-// - enable exact amount checkbox option to autopopulate payment amount with transaction amount
+// - disable payment methods when payment amount is enetered or exact amount checkbox is checked --done
+// - add radio options for payment amounts [exact / 25c / 50c / 1 / 5 / 10 / 20 / other] and disable option if less than amount --done
+// - when exact amount is checked, autocomplete paynment amount input with transaction amount
+// - do NOT show tender amounts with values when a payment method cannot use it - add flags to payment methods
 // - show "Finalizar Transacccion" button when all steps are done
 // - add cancel buttons next to each step to cancel the step
 // - Only show steps 3 & 4 for positive transactions
@@ -18,6 +19,7 @@ export const ACTIONS = {
 	UPDATE_TRANSACTION_AMOUNT: 'UPDATE_TRANSACTION_AMOUNT',
 	SET_PAYMENT_METHOD: 'SET_PAYMENT_METHOD',
 	UPDATE_PAYMENT_AMOUNT: 'UPDATE_PAYMENT_AMOUNT',
+	SET_TENDER_OPTION: 'SET_TENDER_OPTION',
 }
 
 const initialState = {
@@ -26,9 +28,12 @@ const initialState = {
 	isPaymentMethodSelected: false,
 	transactions: TRANSACTIONS,
 	selectedTransaction: null,
+	transactionPaymentMethods: null,
 	paymentMethods: PAYMENT_METHODS,
 	selectedPaymentMethod: null,
-	denominations: TENDER_AMOUNTS,
+	tenderOptions: TENDER_OPTIONS,
+	paymentMethodTenders: null,
+	selectedTenderOption: TENDER_OPTIONS.find(({ id }) => id === 'other'),
 }
 
 const reducer = (state, { type, payload }) => {
@@ -36,12 +41,15 @@ const reducer = (state, { type, payload }) => {
 
 	switch (type) {
 		case ACTIONS.SET_TRANSACTION: {
-			const { id, name, operation } = payload
+			const { icon, ...transaction } = payload
 			return {
 				...state,
 				isTransactionSelected: true,
 				isSetTransactionAmount: false,
-				selectedTransaction: { id, name, operation },
+				selectedTransaction: { ...transaction },
+				transactionPaymentMethods: state.paymentMethods.filter(({ id }) =>
+					transaction?.paymentMethodIds?.includes(id)
+				),
 			}
 		}
 		case ACTIONS.UPDATE_TRANSACTION_AMOUNT: {
@@ -57,16 +65,31 @@ const reducer = (state, { type, payload }) => {
 			}
 		}
 		case ACTIONS.SET_PAYMENT_METHOD: {
-			const { id, name } = payload
+			const { icon, ...paymentMethod } = payload
+			const tenders =
+				paymentMethod.tenderAmountsIds === 1
+					? [...state.tenderOptions]
+					: state.tenderOptions.filter(({ id }) =>
+							paymentMethod?.tenderAmountsIds?.includes(id)
+					  )
 			return {
 				...state,
 				isPaymentMethodSelected: true,
-				selectedPaymentMethod: { id, name },
+				selectedPaymentMethod: { ...paymentMethod },
 				transactions: state.transactions.map(item => ({
 					...item,
 					disabled: true,
 					disabledChecked: item.id === state.selectedTransaction.id,
 				})),
+				paymentMethodTenders: tenders.map(option => {
+					if (
+						'value' in option &&
+						option.value < state.selectedTransaction.amount
+					) {
+						option.disabled = true
+					}
+					return option
+				}),
 			}
 		}
 		case ACTIONS.UPDATE_PAYMENT_AMOUNT: {
@@ -77,11 +100,19 @@ const reducer = (state, { type, payload }) => {
 					...state.selectedPaymentMethod,
 					amount,
 				},
-				paymentMethods: state.paymentMethods.map(item => ({
-					...item,
-					disabled: true,
-					disabledChecked: item.id === state.selectedPaymentMethod.id,
-				})),
+				transactionPaymentMethods: state.transactionPaymentMethods.map(
+					item => ({
+						...item,
+						disabled: true,
+						disabledChecked: item.id === state.selectedPaymentMethod.id,
+					})
+				),
+			}
+		}
+		case ACTIONS.SET_TENDER_OPTION: {
+			return {
+				...state,
+				selectedTenderOption: payload,
 			}
 		}
 		default:
